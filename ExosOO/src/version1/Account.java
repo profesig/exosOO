1a61e96e6ae53a4372c319ec91d42d53b7ff3fe3
Prof Esig <r0g26-githubesig@yahoo.fr>
2013-12-13 22:45:45 +0100

    projet IntelliJ Idea & UML

--- ExosOO/src/version1/Account.java
+++ ExosOO/src/version1/Account.java
@@ -2,24 +2,24 @@ package version1;
 
 
 class Account {
-	
-	private String noCompte;
-	private float solde;
-	
-	Account(String noCompte, float solde) {
-		this.noCompte = noCompte;
-		this.solde = solde;
-	}
-	
-	void crediter(float montant) {
-		solde = solde + montant;
-	}
-	
-	void debiter(float montant) {
-		solde = solde - montant;
-	}
-	
-	public String toString() {
-		return "Account[noCompte: " + noCompte + ", solde: " + solde + "]";
-	}
+
+    private String accountNumber;
+    private float balance;
+
+    Account(String accountNumber, float balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    void credit(float amount) {
+        balance = balance + amount;
+    }
+
+    void debit(float amount) {
+        balance = balance - amount;
+    }
+
+    public String toString() {
+        return "Account[accountNumber: " + accountNumber + ", balance: " + balance + "]";
+    }
 }
--- ExosOO/src/version1/BankingSystem.java
+++ ExosOO/src/version1/BankingSystem.java
@@ -2,26 +2,26 @@ package version1;
 
 
 class BankingSystem {
-	
-	private Account account1;
+
+    private Account account1;
 	private Account account2;
 	
 	BankingSystem() {
 		account1 = new Account("c1", 300);
 		account2 = new Account("c2", 200);	
 	}
-	
-	void virer(float montant, String noCompteDebit, String noCompteCredit) {
-		System.out.println("Nous allons virer un montant de " + montant + " du compte " + noCompteDebit + " à " + noCompteCredit);
-		System.out.println("Etat des comptes avant l'opération:");
-		System.out.println(account1.toString());
-		System.out.println(account2.toString());
-		
-		account1.debiter(montant);
-		account2.crediter(montant);
-		
-		System.out.println("Etat des comptes après l'opération:");
-		System.out.println(account1.toString());
-		System.out.println(account2.toString());
+
+    void transfer(float amount, String debitAccountNumber, String creditAcccountNumber) {
+        System.out.println("Nous allons virer un montant de " + amount + " du compte " + debitAccountNumber + " Ã  " + creditAcccountNumber);
+        System.out.println("Etat des comptes avant l'opÃ©ration:");
+        System.out.println(account1.toString());
+        System.out.println(account2.toString());
+
+        account1.debit(amount);
+        account2.credit(amount);
+
+        System.out.println("Etat des comptes aprÃ¨s l'opÃ©ration:");
+        System.out.println(account1.toString());
+        System.out.println(account2.toString());
 	}
 }
