c864d93b8f2e6902c8276ebf356113efe8843604
Prof Esig <r0g26-githubesig@yahoo.fr>
2013-12-13 22:49:08 +0100

    renommage module exos

--- ExosOO/src/version1/ExosOO_version1.java
+++
@@ -1,9 +0,0 @@
-package version1;
-
-public class ExosOO_version1 {
-    
-    public static void main(String[] args) {
-    	BankingSystem bankingSystem = new BankingSystem();
-        bankingSystem.transfer(50, "c1", "c2");
-    }
-}
---
+++ IntroDiagrammes/src/version1/Account.java
@@ -0,0 +1,25 @@
+package version1;
+
+
+class Account {
+
+    private String accountNumber;
+    private float balance;
+
+    Account(String accountNumber, float balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    void credit(float amount) {
+        balance = balance + amount;
+    }
+
+    void debit(float amount) {
+        balance = balance - amount;
+    }
+
+    public String toString() {
+        return "Account[accountNumber: " + accountNumber + ", balance: " + balance + "]";
+    }
+}
